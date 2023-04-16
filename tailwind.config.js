7cc5505cc9371f9959fb833fe5ef61529d5df7bd
Hazhir Ghafoori <you@example.com>
2023-04-16 19:05:59 +0330

    add fonts to tailwind css, create index.html login form UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,9 @@ module.exports = {
     content: ["./public/index.html", "./src/**/*.{html,js}"],
 
     theme: {
-
+        fontFamily: {
+            'Yekan': ['YekanB'],
+        },
         extend: {},
 
     },
