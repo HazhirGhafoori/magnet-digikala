3f3a11700859f9e649685d8b24911d59a9e2ccba
Hazhir Ghafoori <you@example.com>
2023-04-16 21:51:41 +0330

    checked if user not entered password,and show a text that dont entered password!

--- src/js/js.js
+++ src/js/js.js
@@ -1,10 +1,17 @@
 window.onload = function() {
     let username, password;
 
-    document.getElementById("signup").onclick = function() {
 
+
+    document.getElementById("signup").onclick = function() {
         username = document.getElementById("username").value;
         password = document.getElementById("password").value;
+        password = String(password);
+
+        if (password == '') {
+            document.getElementById("enterPassword").innerHTML = 'لطفا پسورد را وارد نمایید!';
+        }
+
 
         console.log("Your User Name is: " + username + " Your password is: " + password);
         console.log("ok");
