7ce008d92573acd51e5e2b774a3cd43784bae970
Hazhir Ghafoori <you@example.com>
2023-04-16 19:21:30 +0330

    add js file to index.html, get username && password and show in console.log

---
+++ src/js/js.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+    let username, password;
+
+    document.getElementById("signup").onclick = function() {
+
+        username = document.getElementById("username").value;
+        password = document.getElementById("password").value;
+
+        console.log("Your User Name is: " + username + " Your password is: " + password);
+        console.log("ok");
+
+
+
+
+    }
+
+}
