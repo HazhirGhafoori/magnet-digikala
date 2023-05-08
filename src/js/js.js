a06c3c5fa46ef65209827555225d2fbda2018e14
Hazhir Ghafoori <you@example.com>
2023-05-08 07:05:53 +0330

    add selectBox for Date of Live by beautifull Calender!

--- src/js/js.js
+++ src/js/js.js
@@ -219,7 +219,17 @@ window.onload = function() {
         i = i.replace("/", "");
         return i;
     }
+    mobiscroll.setOptions({
+        theme: 'ios',
+        themeVariant: 'light'
+    });
 
+    mobiscroll.datepicker('#DateOfLive', {
+        locale: mobiscroll.localeFa,
+        controls: ['date'],
+        rtl: true,
+        display: 'inline'
+    });
 
 
 };
